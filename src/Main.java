f213073ca9252f58388cbddcb2c6ef69227a3f5a
felipe <119119348+felipekimur4@users.noreply.github.com>
2024-04-14 21:26:47 -0300

    Update Main.java
    Refatorado codigo de forma em que ele fique mais legivel

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,10 @@ public class Main {
     public static void main(String[] args) {
         float n1;
         float n2;
-        float total;
+        float total = 0;
         String operacao;
         boolean reiniciar;
         String verificaLoop;
-
         Scanner ler = new Scanner(System.in);
 
 
@@ -20,64 +19,34 @@ public class Main {
             System.out.print("Digite o segundo número: ");
             n2 = ler.nextFloat();
 
-            if ( operacao.equals("Divisao") ) {
-                if ( n1 == 0 && n2 == 0 ) {
+            if (operacao.equals("Divisao")) {
+                if (n1 == 0 && n2 == 0) {
                     System.out.println("Não é possivel dividir 0 por ele mesmo.\n");
-
                 } else {
                     total = n1 / n2;
-                    System.out.printf("O seu total é %.2f\n", total);
-                    System.out.println("Deseja fazer outro  calculo?(sim ou nao)");
-                    verificaLoop = ler.next();
-                    if ( verificaLoop.equals("sim")) {
-                        reiniciar = true;
-                    } else {
-                        reiniciar = false;
-                    }
                 }
-            } else if ( operacao.equals("Multiplicacao") ) {
+            } else if (operacao.equals("Multiplicacao")) {
                 total = n1 * n2;
-                System.out.printf("O seu total é %.2f\n", total);
-                System.out.println("Deseja fazer outro  calculo?(sim ou nao)");
-                verificaLoop = ler.next();
-                if ( verificaLoop.equals("sim")) {
-                    reiniciar = true;
-                } else {
-                    reiniciar = false;
-                }
 
-            } else if ( operacao.equals("Somar") ) {
+            } else if (operacao.equals("Somar")) {
                 total = n1 + n2;
-                System.out.printf("O seu total é %.2f\n", total);
-                System.out.println("Deseja fazer outro  calculo?(sim ou nao)");
-                verificaLoop = ler.next();
-                if ( verificaLoop.equals("sim")) {
-                    reiniciar = true;
-                } else {
-                    reiniciar = false;
-                }
-            } else if ( operacao.equals("Diminuir\n") ) {
+
+            } else if (operacao.equals("Diminuir\n")) {
                 total = n1 - n2;
-                System.out.printf("O seu total é %.\n", total);
-                System.out.println("Deseja fazer outro  calculo?(sim ou nao)");
-                verificaLoop = ler.next();
-                if ( verificaLoop.equals("sim")) {
-                    reiniciar = true;
-                } else {
-                    reiniciar = false;
-                }
+
             } else {
                 System.out.println("Operação inválida\n");
                 System.out.println("Deseja fazer outro  calculo?(sim ou nao)");
-                verificaLoop = ler.next();
-                if ( verificaLoop.equals("sim")) {
-                    reiniciar = true;
-                } else {
-                    reiniciar = false;
-                }
             }
+            System.out.printf("O seu total é %.2f\n", total);
+            System.out.println("Deseja fazer outro  calculo?(sim ou nao)");
+            verificaLoop = ler.next();
+            if (verificaLoop.equals("sim")) {
+                reiniciar = true;
+            } else {
+                reiniciar = false;
 
-
-        } while (reiniciar == true);
+            }
+        } while (reiniciar) ;
     }
 }
